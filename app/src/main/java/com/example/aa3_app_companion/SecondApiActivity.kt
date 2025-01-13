a1c2fdf5b9af2e4dfb2fe9c48a449d80d121dcb1
Marcos <marcos.ruiz@enti.cat>
2025-01-13 14:49:48 +0100

    Api fixed

--- app/src/main/java/com/example/aa3_app_companion/SecondApiActivity.kt
+++ app/src/main/java/com/example/aa3_app_companion/SecondApiActivity.kt
@@ -3,8 +3,11 @@ package com.example.aa3_app_companion
 import CelesteApi.CelesteApiInstance
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import models.CelesteResponse
 import models.ClassesResponse
 import retrofit2.Call
@@ -12,10 +15,16 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class SecondApiActivity : AppCompatActivity() {
+
+    private  lateinit var toolbar : Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second_api)
+
+        toolbar = findViewById(R.id.apibar)
+        setSupportActionBar(toolbar)
 
         callApi()
     }
@@ -48,5 +57,20 @@ class SecondApiActivity : AppCompatActivity() {
                 Log.d("Api","on Failure")
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_class_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.arrow -> {
+                onBackPressed()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
